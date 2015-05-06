e2f4dd4e4d108bd8322ec03149748d8b064f79f0
Anders Lundstedt <anders@anderslundstedt.se>
2015-05-06 17:22:02 +0200

    Coqdoc and restructuring.

--- coq/myvec.v
+++ coq/myvec.v
@@ -1,6 +1,38 @@
+(**
+* A vector library
+I am probably more or less reinventing the wheel here,
+but I found the standard library's vector implementations
+([Coq.Vectors.*])
+insufficient for two reasons:
+- Vectors as separate inductive types,
+  instead of defined as functions to [Type],
+  with which a lot would come for free by computation
+  and with which induction proofs would be easier
+  (cf. %\url{https://homes.cs.washington.edu/~jrw12/dep-destruct.html}%).
+- All functions are defined under the interpretation that
+  vectors are built by appending a vector to an element.
+  For my purposes the reverse interpretation---
+  that vectors are built by appending an element to a vector---
+  is more suitable.
+*)
+
 Require Import Coq.Classes.Morphisms.
 
+(**
+** Canonical finite vectors
+This module defines the canonical $n$-element vectors
+$(0,\dotsc,n-1)$.
+*)
+
 Module FIN.
+
+(**
+*** Definition
+The $0$-element vector $()$ is the empty type.
+The $(n+1)$-element vector $(0,\dotsc,n)$ is
+the sum of the $n$-element vector and
+the singleton of $n$.
+*)
 
   Inductive Singleton (n : nat) := lift :> Singleton n.
 
@@ -11,6 +43,22 @@ Module FIN.
 
   Definition last {n} : t (S n) := inr (lift n).
 
+  Lemma t0Empty (x : t 0) : False.
+  Proof. destruct x. Qed.
+
+  Lemma t1Singleton (x : t 1) : x = last.
+  Proof. destruct x as [x | x]; destruct x. reflexivity. Qed.
+
+(**
+An element of
+$(0,\dotsc,n-1)$
+is also an element of
+$(0,\dotsc,n)$.
+More generally,
+it is also an element of
+$(0,\dotsc,k+n-1)$.
+*)
+
   Definition up {n} (x : t n) : t (S n) := inl x.
   Local Notation "++ x" := (up x) (at level 6, right associativity).
 
@@ -19,14 +67,11 @@ Module FIN.
   | S k => ++(up' x)
   end.
 
+(**
+*** Functions to and from naturals
+*)
 
   Definition ofNat (n k : nat) : t (k + S n) := up' last.
-
-  Lemma t0Empty (x : t 0) : False.
-  Proof. destruct x. Qed.
-
-  Lemma t1Singleton (x : t 1) : x = last.
-  Proof. destruct x as [x | x]; destruct x. reflexivity. Qed.
 
   Fixpoint toNat {n} : t (S n) -> nat := match n with
   | O   => fun x => 0
@@ -36,6 +81,18 @@ Module FIN.
                     end
   end.
 
+(**
+Now one would want to prove
+the correctness of [ofNat] and [toNat],
+i.e. that [ofNat (toNat x) k = up' x] and
+that [toNat (ofNat n k) = n].
+It is however nontrivial to even state these,
+since [(k + S n) = (S _)] does not automatically typecheck.
+For my purposes,
+I do not need to prove their correctness so
+I will skip doing so.
+*)
+
   Module NOTATIONS.
     Delimit Scope FIN with FIN.
     Notation "++ x" := ++x (at level 6, right associativity) : FIN.
@@ -43,10 +100,22 @@ Module FIN.
 
 End FIN.
 
+(**
+** Vectors
+This module defines finite homogeneous vectors of elements of arbitrary type.
+*)
+
 Module VEC.
 
   Import FIN.NOTATIONS.
   Local Open Scope FIN.
+
+(**
+*** Definition
+The type of [A]-vectors of length $0$ is the unit type.
+The type of [A]-vectors of length $n+1$ is
+the product of the type of $n$-element vectors and [A].
+*)
 
   Fixpoint t A (n : nat) : Type := match n with
   | O   => unit
@@ -59,6 +128,10 @@ Module VEC.
     ((fun A n (v : t A n) (a : A) => ((v, a) : t A (S n))) _ _)
     (at level 40, left associativity).
 
+(**
+*** Accessing elements of a vector
+*)
+
   Fixpoint nth {A} {n} : t A n -> FIN.t n -> A := match n with
   | O   => fun _ x => False_rect _ (FIN.t0Empty x)
   | S n => fun v x => let (v, a) := v in match x with
@@ -66,6 +139,10 @@ Module VEC.
                       | inr _ => a
                       end
   end.
+
+(**
+*** Pointwise equal vectors are equal
+*)
 
   Lemma pointwiseEquality {A} {n} (v w : t A n) :
     (forall x : FIN.t n, nth v x = nth w x) <-> v = w.
@@ -81,8 +158,70 @@ Module VEC.
       intro x. exact (H (++x)).
   Qed.
 
+(**
+*** Vectors of copies an element
+*)
+
+  Fixpoint copies {A} (n : nat) (a : A) : t A n := match n with
+  | O   => ()
+  | S n => (copies n a, a)
+  end.
+
+  Lemma nthCopies {A} {n} (a : A) (x : FIN.t n) : nth (copies n a) x = a.
+  Proof.
+    induction n as [ | n IHn]; simpl.
+    - inversion x.
+    - destruct x as [x | x].
+      + apply IHn.
+      + reflexivity.
+  Qed.
+
+(**
+*** Exactly one empty vector
+*)
+
   Lemma uniqueEmptyVector {A} (v : t A 0) : v = tt.
   Proof. destruct v. reflexivity. Qed.
+
+(**
+*** Concatenation of vectors
+*)
+
+  Fixpoint concat {A} {n m} (v : t A n) : t A m -> t A (m+n) :=
+  match m with
+  | O   => fun _ => v
+  | S m => fun w => let (w, a) := w in (concat v w, a)
+  end.
+
+(**
+*** Head, tail, init and last
+*)
+
+  Fixpoint head {A} {n} : t A (S n) -> A := match n with
+  | O   => fun v => let (_, a) := v in a
+  | S n => fun v => let (v, _) := v in head v
+  end.
+
+  Fixpoint tail {A} {n} : t A (S n) -> t A n := match n with
+  | O   => fun _ => tt
+  | S n => fun v => let (v, a) := v in (tail v, a)
+  end.
+
+  Definition init {A} {n} (v : t A (S n)) : t A n := let (v, _) := v in v.
+
+  Definition last {A} {n} (v : t A (S n)) : A := let (_, a) := v in a.
+
+  Lemma initLastEq {A} {n} (v : t A (S n)) : v = (init v, last v).
+  Proof. destruct v as [v a]. reflexivity. Qed.
+
+(**
+*** Mapping functions on vectors
+The map of [f : A -> B] over a vector
+$(a_0,\dotsc,a_{n-1})$ of elements of type [A]
+is the vector
+$(f(a_0),\dotsc,f(a_{n-1}))$,
+with elements of type [B].
+*)
 
   Fixpoint map {A B} {n} (f : A -> B) : t A n -> t B n := match n with
   | O   => fun _ => ()
@@ -119,6 +258,10 @@ Module VEC.
       + apply IHn.
       + reflexivity.
   Qed.
+
+(**
+*** The image of functions on finite vectors as vectors
+*)
 
   Fixpoint finMap {A} {n} : (FIN.t n -> A) -> t A n := match n with
   | O   => fun _ => ()
@@ -160,19 +303,6 @@ Module VEC.
     map g (finMap' f) = finMap' (fun x => g (f x)).
   Proof. unfold finMap'. apply finMapCompose. Qed.
 
-  Definition init {A} {n} (v : t A (S n)) : t A n := let (v, _) := v in v.
-
-  Definition last {A} {n} (v : t A (S n)) : A := let (_, a) := v in a.
-
-  Lemma initLastEq {A} {n} (v : t A (S n)) : v = (init v, last v).
-  Proof. destruct v as [v a]. reflexivity. Qed.
-
-  Fixpoint concat {A} {n m} (v : t A n) : t A m -> t A (m+n) :=
-  match m with
-  | O   => fun _ => v
-  | S m => fun w => let (w, a) := w in (concat v w, a)
-  end.
-
   Lemma concatMap {A B} {n m} (f : A -> B) (v : VEC.t A n) (w : VEC.t A m) :
     map f (concat v w) = concat (map f v) (map f w).
   Proof.
@@ -181,15 +311,9 @@ Module VEC.
     - destruct w as [w a]. rewrite IHm. reflexivity.
   Qed.
 
-  Fixpoint head {A} {n} : t A (S n) -> A := match n with
-  | O   => fun v => let (_, a) := v in a
-  | S n => fun v => let (v, _) := v in head v
-  end.
-
-  Fixpoint tail {A} {n} : t A (S n) -> t A n := match n with
-  | O   => fun _ => tt
-  | S n => fun v => let (v, a) := v in (tail v, a)
-  end.
+(**
+*** Folds
+*)
 
   Fixpoint foldL {A} {n} (f : A -> A -> A) : A -> t A n -> A := match n with
   | O   => fun a _ => a
@@ -208,20 +332,6 @@ Module VEC.
     induction m as [ | m IHm]; simpl.
     - reflexivity.
     - destruct w as [w b]. rewrite IHm. reflexivity.
-  Qed.
-
-  Fixpoint copies {A} (n : nat) (a : A) : t A n := match n with
-  | O   => ()
-  | S n => (copies n a, a)
-  end.
-
-  Lemma nthCopies {A} {n} (a : A) (x : FIN.t n) : nth (copies n a) x = a.
-  Proof.
-    induction n as [ | n IHn]; simpl.
-    - inversion x.
-    - destruct x as [x | x].
-      + apply IHn.
-      + reflexivity.
   Qed.
 
   Module NOTATIONS.
