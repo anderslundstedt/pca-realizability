c83d68e99b8684559d04203f71a90edb0b682e73
Anders Lundstedt <anders@anderslundstedt.se>
2015-08-19 03:43:16 +0200

    simplification, removed one exported definition

--- coq/normaltermpca.v
+++ coq/normaltermpca.v
@@ -45,27 +45,6 @@ Module NORMAL_TERM_PCA.
 
   Definition Appl (U V W : normalTerm) : Prop := U⋅V >>* W.
 
-  Fixpoint closedPasTermToTerm `{_ : PAS.Pas normalTerm} (t : PAS.term 0)
-    : term :=
-  match t with
-  | &a        => let (a, _) := a in a
-  | PAS.var x => False_rect _ (FIN.THMS.t0Empty x)
-  | u*v       => closedPasTermToTerm u ⋅ closedPasTermToTerm v
-  end.
-  Local Notation "§ t" := (closedPasTermToTerm t) (at level 6).
-
-  (**
-  ** Notations
-  *)
-
-  Module NOTATIONS.
-
-    Delimit Scope NT_PCA with NT_PCA.
-
-    Notation "§ t" := (§t) (at level 6) : NT_PCA.
-
-  End NOTATIONS.
-
   (**
   ** Theorems
   *)
@@ -78,11 +57,10 @@ Module NORMAL_TERM_PCA.
 
     Axiom nonTriviality : PCA.NonTrivial pca.
 
-    Axiom closedPasTermDenotation :
-      forall (u : PAS.term 0) (T : normalTerm), u ≃ &T <-> §u >>* T.
+    Axiom normalTermDenotation : forall (T : normalTerm), §T ≃ &T.
 
-    Axiom eagerlyReduces_closedPasTermEq :
-      forall u v : PAS.term 0, §u >>* §v -> u ≃ v.
+    Axiom termDenotation :
+      forall (U : term) (V : normalTerm), §U ≃ &V <-> U >>* V.
 
   End THMS_SIG.
 
@@ -102,44 +80,69 @@ Module NORMAL_TERM_PCA.
         eapply strictNormalFormsUnique; unfold SNF; eauto.
     Qed.
 
-    Theorem closedPasTermDenotation (u : PAS.term 0) (T : normalTerm) :
-      u ≃ &T <-> §u >>* T.
+    Fixpoint termToClosedPasTerm (T : term) : PAS.term 0 :=
+    match T with
+    | KS_RW.K   => &K
+    | KS_RW.S   => &S
+    | U⋅V       => termToClosedPasTerm U * termToClosedPasTerm V
+    end.
+    Local Notation "$ t" := (termToClosedPasTerm t) (at level 6).
+
+    Lemma termDenotation' (U : term) (V : normalTerm) :
+      $(U) ≃ &V <-> U >>* V.
     Proof.
-      revert T. induction u as [U | x | u IHu v IHv]; intro T.
-      - assert (&U ≃ &T <-> U == T) as H. {
+      revert V. induction U as [ | | U IHU V IHV]; intro T.
+      - simpl.
+        assert (&K ≃ &T <-> K == T) as H. {
           split; intro H.
           - apply PAS.THMS.constInjective. exact H.
           - rewrite H. reflexivity.
         }
-        rewrite H. clear H. simpl. destruct U as [U HU], T as [T HT].
-        unfold NormalTermEq. simpl. split; intro H.
-        + rewrite H. apply rt_refl.
-        + apply strictlyNormalEagerReduction; assumption.
-      - contradiction.
+        rewrite H. clear H. simpl. unfold NormalTermEq. split; intro H.
+        + rewrite <- H. simpl. apply rt_refl.
+        + destruct T as [T HT]. simpl in *.
+          apply strictlyNormalEagerReduction.
+          * apply kStrictlyNormal.
+          * exact H.
+      - simpl.
+        assert (&S ≃ &T <-> S == T) as H. {
+          split; intro H.
+          - apply PAS.THMS.constInjective. exact H.
+          - rewrite H. reflexivity.
+        }
+        rewrite H. clear H. simpl. unfold NormalTermEq. split; intro H.
+        + rewrite <- H. simpl. apply rt_refl.
+        + destruct T as [T HT]. simpl in *.
+          apply strictlyNormalEagerReduction.
+          * apply sStrictlyNormal.
+          * exact H.
       - split; intro H.
-        + apply PAS.THMS.correctness1 in H. destruct H as [U [V [Hu [Hv H]]]].
-          unfold Appl in H. simpl.
-          specialize (IHu U). destruct IHu as [IHu _]. specialize (IHu Hu).
-          specialize (IHv V). destruct IHv as [IHv _]. specialize (IHv Hv).
-          clear Hu Hv. apply rt_trans with (y := U⋅V).
-          * destruct U as [U HU], V as [V HV]. simpl in *.
+        + simpl in H. apply PAS.THMS.correctness1 in H.
+          destruct H as [U' [V' [HU [HV H]]]].
+          unfold Appl in H.
+          specialize (IHU U'). destruct IHU as [IHU _]. specialize (IHU HU).
+          specialize (IHV V'). destruct IHV as [IHV _]. specialize (IHV HV).
+          clear HU HV. apply rt_trans with (y := U'⋅V').
+          * destruct U' as [U' HU'], V' as [V' HV']. simpl in *.
             apply eagerReductionPar; assumption.
           * exact H.
-        + assert (SNF (§u⋅§v) T) as H'. {
+        + simpl.
+          assert (SNF (U⋅V) T) as H'. {
             split.
             - exact H.
             - destruct T as [T HT]. exact HT.
           }
           destruct
-            (strictNormalFormApplL _ _ _ H') as [U [Hu HU]],
-            (strictNormalFormApplR _ _ _ H') as [V [Hv HV]].
+            (strictNormalFormApplL _ _ _ H') as [U' [HU HU']],
+            (strictNormalFormApplR _ _ _ H') as [V' [HV HV']].
           clear H'.
-          set (U' := exist _ _ HU : normalTerm).
-          set (V' := exist _ _ HV : normalTerm).
-          specialize (IHu U'). destruct IHu as [_ IHu]. rewrite (IHu Hu).
-          specialize (IHv V'). destruct IHv as [_ IHv]. rewrite (IHv Hv).
-          rewrite PAS.THMS.correctness1'. unfold Appl. simpl. clear IHu IHv U' V'.
-          pose proof (eagerReductionPar _ _ _ _ HU Hu Hv) as H'.
+          set (U'' := exist _ _ HU' : normalTerm).
+          set (V'' := exist _ _ HV' : normalTerm).
+          specialize (IHU U''). destruct IHU as [_ IHU]. rewrite (IHU HU).
+          specialize (IHV V''). destruct IHV as [_ IHV]. rewrite (IHV HV).
+          rewrite PAS.THMS.correctness1'. unfold Appl. simpl.
+          clear IHU IHV U'' V''.
+          pose proof (eagerReductionPar _ _ _ _ HU' HU HV) as H'.
           destruct
             (RW.THMS.functionalDoubleReduction
               EagerlyOneStepReduces eagerOneStepReductionFunctional _ _ _ H H')
@@ -150,10 +153,14 @@ Module NORMAL_TERM_PCA.
           apply rt_refl.
     Qed.
 
-    Theorem eagerlyReduces_closedPasTermEq (u v : PAS.term 0) :
-      §u >>* §v -> u ≃ v.
+    Lemma normalTermDenotation' (T : normalTerm) : $(T) ≃ &T.
+    Proof. setoid_rewrite termDenotation'. apply rt_refl. Qed.
+
+    Lemma eagerlyReduces_closedPasTermEq (U V : term) :
+      U >>* V -> $(U) ≃ $(V).
     Proof.
-      intro H. rewrite PAS.THMS.correctness2. setoid_rewrite closedPasTermDenotation.
+      intro H. rewrite PAS.THMS.correctness2.
+      setoid_rewrite termDenotation'.
       intros [T HT]. simpl. split; intro H'.
       - destruct (eagerReductionConfluent _ _ _ H H') as [T' [H'' HT']].
         apply (strictlyNormalEagerReduction _ _ HT) in HT'.
@@ -164,16 +171,26 @@ Module NORMAL_TERM_PCA.
     Instance pca : PCA.Pca pas K S.
     Proof.
       split.
-      - setoid_rewrite closedPasTermDenotation. intros [U HU] [V HV]. simpl.
-        apply rt_step.
-        unfold EagerlyOneStepReduces. simpl. rewrite HU, HV.
+      - intros U V.
+        setoid_rewrite <- normalTermDenotation'.
+        cutrewrite ($(K)*$(U)*$(V) = $(K⋅U⋅V)); [ | reflexivity].
+        apply eagerlyReduces_closedPasTermEq.
+        destruct U as [U HU], V as [V HV]. simpl.
+        apply rt_step. unfold EagerlyOneStepReduces. simpl. rewrite HU, HV.
         reflexivity.
-      - unfold PAS.Denotes. setoid_rewrite closedPasTermDenotation.
-        intros [U HU] [V HV]. simpl.
+      - intros U V.
+        setoid_rewrite <- normalTermDenotation'.
+        cutrewrite ($(S)*$(U)*$(V) = $(S⋅U⋅V)); [ | reflexivity].
+        unfold PAS.Denotes. setoid_rewrite termDenotation'.
+        destruct U as [U HU], V as [V HV]. simpl.
         set (SUV := exist _ _ (suvStrictlyNormal _ _ HU HV) : normalTerm).
         exists SUV. apply rt_refl.
-      - intros [U HU] [V HV] [W HW].
-        apply eagerlyReduces_closedPasTermEq. simpl.
+      - intros U V W.
+        setoid_rewrite <- normalTermDenotation'.
+        cutrewrite ($(S)*$(U)*$(V)*$(W) = $(S⋅U⋅V⋅W)); [ | reflexivity].
+        cutrewrite ($(U)*$(W)*($(V)*$(W)) = $(U⋅W⋅(V⋅W))); [ | reflexivity].
+        apply eagerlyReduces_closedPasTermEq.
+        destruct U as [U HU], V as [V HV], W as [W HW]. simpl.
         apply rt_step.
         unfold EagerlyOneStepReduces. simpl. rewrite HU, HV, HW.
         reflexivity.
@@ -181,6 +198,20 @@ Module NORMAL_TERM_PCA.
 
     Theorem nonTriviality : PCA.NonTrivial pca.
     Proof. exists K, S. intros H. discriminate H. Qed.
+
+    Lemma equalMaps (T : term) : §T = $(T).
+    Proof.
+      induction T as [ | | U IHU V IHV].
+      - reflexivity.
+      - reflexivity.
+      - simpl. rewrite IHU, IHV. reflexivity.
+    Qed.
+
+    Theorem normalTermDenotation (T : normalTerm) : §T ≃ &T.
+    Proof. rewrite equalMaps. apply normalTermDenotation'. Qed.
+
+    Theorem termDenotation (U : term) (V : normalTerm) : §U ≃ &V <-> U >>* V.
+    Proof. rewrite equalMaps. apply termDenotation'. Qed.
 
   End THMS.
 
